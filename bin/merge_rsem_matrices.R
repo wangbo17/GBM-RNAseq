92d84d7e221c4a5e4c3729b860fee9af110866e5
B Wang <gdmn373@login2.arc4.leeds.ac.uk>
2025-05-09 09:32:34 +0100

    Make R script executable

--- bin/merge_rsem_matrices.R
+++ bin/merge_rsem_matrices.R
@@ -14,35 +14,35 @@ isoform_files <- args[isoforms_start:isoforms_end]
 library(readr)
 library(dplyr)
 
-read_matrix <- function(files) {
-  gene_ids <- NULL
+read_matrix <- function(files, id_col) {
+  ids <- NULL
   expected_count_list <- list()
   TPM_list <- list()
   FPKM_list <- list()
 
   for (file in files) {
-    sample_name <- gsub("_Aligned.(genes|isoforms).results", "", basename(file))
+    sample_name <- gsub("_Aligned\\.(genes|isoforms)\\.results", "", basename(file))
     df <- read_tsv(file, col_types = cols()) %>%
-      select(gene_id, expected_count, TPM, FPKM)
+      select(all_of(id_col), expected_count, TPM, FPKM)
 
-    if (is.null(gene_ids)) gene_ids <- df$gene_id
+    if (is.null(ids)) ids <- df[[id_col]]
     expected_count_list[[sample_name]] <- df$expected_count
     TPM_list[[sample_name]] <- df$TPM
     FPKM_list[[sample_name]] <- df$FPKM
   }
 
   list(
-    expected_count = as.data.frame(cbind(gene_id = gene_ids, do.call(cbind, expected_count_list))),
-    TPM = as.data.frame(cbind(gene_id = gene_ids, do.call(cbind, TPM_list))),
-    FPKM = as.data.frame(cbind(gene_id = gene_ids, do.call(cbind, FPKM_list)))
+    expected_count = as.data.frame(cbind(!!id_col := ids, do.call(cbind, expected_count_list))),
+    TPM            = as.data.frame(cbind(!!id_col := ids, do.call(cbind, TPM_list))),
+    FPKM           = as.data.frame(cbind(!!id_col := ids, do.call(cbind, FPKM_list)))
   )
 }
 
 write_matrix <- function(matrix_list, prefix) {
   write_tsv(matrix_list$expected_count, paste0(prefix, "_expected_count_matrix.tsv"))
-  write_tsv(matrix_list$TPM, paste0(prefix, "_TPM_matrix.tsv"))
-  write_tsv(matrix_list$FPKM, paste0(prefix, "_FPKM_matrix.tsv"))
+  write_tsv(matrix_list$TPM,            paste0(prefix, "_TPM_matrix.tsv"))
+  write_tsv(matrix_list$FPKM,           paste0(prefix, "_FPKM_matrix.tsv"))
 }
 
-write_matrix(read_matrix(gene_files), "gene")
-write_matrix(read_matrix(isoform_files), "isoform")
+write_matrix(read_matrix(gene_files, "gene_id"), "gene")
+write_matrix(read_matrix(isoform_files, "transcript_id"), "isoform")
